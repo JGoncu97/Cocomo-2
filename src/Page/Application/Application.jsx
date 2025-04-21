9ca6bc4a6db5204d924cbec77049df85217368ea
JGoncu97 <jggc1197@gmail.com>
2025-04-21 07:28:06 -0500

    Fix1

--- src/Page/Application/Application.jsx
+++ src/Page/Application/Application.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { CocomoCalculator } from '../../Components/CocomoCalculator'
+import {CocomoCalculator} from '../../Components/CocomoCalculator'
 import { Header } from '../../Components/Header/Header'
 import { Footer } from '../../Components/Footer/Footer'
 
