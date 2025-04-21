a082511f809ae9d89c5bb9223336e027d41b2c71
JGoncu97 <jggc1197@gmail.com>
2025-04-21 07:10:32 -0500

    Arreglo de archivo de imagen

--- src/Page/Info/Developers.jsx
+++ src/Page/Info/Developers.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ImgJose from '../../Assets/jose.jpeg';
 import ImgJoan from '../../Assets/joan.jpeg';
-import GitHub from '../../Assets/GitHub2.jpg';
+import GitHub from '../../Assets/Github2.jpg';
 import {Header} from '../../Components/Header/Header'
 import {Footer} from '../../Components/Footer/Footer'
 
